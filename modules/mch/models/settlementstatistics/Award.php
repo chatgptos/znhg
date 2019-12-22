aabda5097abc6ebbab4a6425d2dd886123cb4339
yanglin <yanglin@baitangla.cn>
2019-12-22 22:47:15 +0800

    setting_goods

--- modules/mch/models/settlementstatistics/Award.php
+++ modules/mch/models/settlementstatistics/Award.php
@@ -26,7 +26,7 @@ class Award extends \yii\db\ActiveRecord
      */
     public static function tableName()
     {
-        return '{{%award}}';
+        return '{{%settlementbonus_goods_setting}}';
     }
 
     /**
