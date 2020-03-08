b3c22a775e72df302a6fa1070624d043932a02f1
yanglin <yanglin@baitangla.cn>
2020-03-08 08:51:56 +0800

    jifen

--- models/IntegralLog.php
+++ models/IntegralLog.php
@@ -38,8 +38,8 @@ class IntegralLog extends \yii\db\ActiveRecord
     {
         return [
             [['user_id', 'content', 'addtime', 'username', 'operator', 'store_id', 'operator_id'], 'required'],
-            [['user_id', 'addtime', 'store_id', 'operator_id'], 'integer'],
-            [['integral','content'], 'string'],
+            [['user_id', 'addtime', 'store_id', 'operator_id','integral'], 'integer'],
+            [['content'], 'string'],
             [['username', 'operator'], 'string', 'max' => 255],
         ];
     }
