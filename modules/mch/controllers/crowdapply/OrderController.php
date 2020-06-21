28061cddcf1eeb179ab837bc1b70445f8ff4f311
yanglin <yanglin@baitangla.cn>
2020-06-22 05:39:57 +0800

    zhibojian

--- modules/mch/controllers/crowdapply/OrderController.php
+++ modules/mch/controllers/crowdapply/OrderController.php
@@ -135,7 +135,7 @@ class OrderController extends Controller
         $form = new OrderClerkForm();
         $form->order_id = \Yii::$app->request->get('order_id');
         $form->price = \Yii::$app->request->get('price');
-        $form->price = \Yii::$app->request->get('type');
+        $form->type = \Yii::$app->request->get('type');
         $form->store_id = $this->store->id;
         $this->renderJson($form->save());
     }
