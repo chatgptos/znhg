3f5bf9872dd92ccc2d372cc4297e5311f42150ab
yanglin <yanglin@baitangla.cn>
2020-06-23 12:14:56 +0800

    zhibojian

--- modules/mch/models/crowdapply/OrderClerkForm.php
+++ modules/mch/models/crowdapply/OrderClerkForm.php
@@ -8,7 +8,9 @@
 namespace app\modules\mch\models\crowdapply;
 
 
+use app\models\Room;
 use app\models\User;
+use app\modules\api\models\BusinessCommentForm;
 use app\modules\mch\models\Model;
 
 /**
@@ -40,7 +42,7 @@ class OrderClerkForm extends Model
                 'msg'=>'网络异常-1'
             ];
         }
-        $user = User::findOne(['id'=>$this->user_id]);
+        $user = User::findOne(['id'=>$order->user_id]);
         if( $this->price == 0){
             return [
                 'code'=>1,
@@ -56,6 +58,41 @@ class OrderClerkForm extends Model
         $order->clerk_id = 9999;//后台核销
         $order->shop_id = 9999;//后台核销
         $order->room_id = $this->price;
+
+
+
+        $room_info =  Room::findOne(['room_id' => $order->room_id, 'store_id' => $this->store_id ,'is_delete' =>0]);
+        if(!$room_info){
+            return [
+                'code'=>1,
+                'msg'=>'请同步腾讯直播间'
+            ];
+        }
+
+        $coupon=1;//赠送券
+        //新人增加
+        User::updateAll(
+            ['coupon'=>$user->coupon+$coupon],
+            ['id' => $order->user_id]
+        );
+        //增加一张券
+        $form = new BusinessCommentForm();
+        $form->user_id = $order->user_id;
+        $form->store_id = 1;
+        $form->num = 1;
+        $form->title = $room_info->name;
+        $form->room_id = $room_info->room_id;//是购值爽服务点 购值爽服务点表象
+        $form->good_id = 0;//是购值爽服务点 购值爽服务点表象
+        $form->article_id = 0;//是购值爽服务点 购值爽服务点表象
+        $res1 = $form->add();
+
+        if(!$res1){
+            return [
+                'code'=>1,
+                'msg'=>'创建直播间发现板块失败'
+            ];
+        }
+
         if($this->type==2){
             $order->is_use = 1;
         }else{
